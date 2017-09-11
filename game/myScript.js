0ac65ef1541ebb5beafd09ae54d36ca8ef5fa4b1
Timothy Tran <timothyhongtran@berkeley.edu>
2017-09-11 00:53:18 -0700

    Added Files
    Added Files

---
+++ game/myScript.js
@@ -0,0 +1,22 @@
+var myScene, myBackground, myCharacter, myText, myMusic, mySoundEffect; 
+var sceneArray; //each element of the array will contain each of the above elements
+
+// var audio = new Audio('shake.mp3');
+// audio.play();
+
+function changeBackground() {
+    document.getElementById("background").src = "images/backgrounds/ethanApartment.jpg";
+}
+
+function changeCharacter() {
+}
+
+function changeText() {
+}
+
+function changeMusic() {
+}
+
+function changeSoundEffect() {	
+}
+
